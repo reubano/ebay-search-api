cc2506a0cce354622d8b122d3b237563919d435a
Reuben Cummings <reubano@gmail.com>
2014-04-29 16:30:18 +0300

    Bump to version 1.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ config = {
 	'long_description': open('README.rst', 'rt').read(),
 	'author': 'Reuben Cummings',
 	'author_email': 'reubano@gmail.com',
-	'version': '1.1.0',
+	'version': '1.1.1',
 	'install_requires': requirements.split('\n'),
 	'classifiers': [
 		'Development Status :: 4 - Beta',
